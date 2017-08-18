45a5e0edc16b2f3274cec0f4a033ecdf6375cd6d
Franklyn Zhu <zfranklyn@gmail.com>
2017-08-18 23:52:29 +0800

    removed title

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,6 @@ class App extends React.Component<{}, {}> {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
