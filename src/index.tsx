ed62895901508ec4f7638c5f480897c55b770030
Franklyn Zhu <zfranklyn@gmail.com>
2017-10-16 23:43:25 -0400

    removed node_modules dependency from tsx file

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import * as ReactDOM from 'react-dom';
 import AppContainer from './components/AppContainer';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
-import '../node_modules/@blueprintjs/core/dist/blueprint.css';
 
 ReactDOM.render(
   <AppContainer />,
